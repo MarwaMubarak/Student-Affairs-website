70308801100e54bde2d7aec9d5e6088e830f815a
Marwa Mubarak <marwamubarak6@gmail.com>
2022-04-30 01:30:00 +0200

    Update login_JS.js

--- JS/login_JS.js
+++ JS/login_JS.js
@@ -3,7 +3,7 @@ function LoginValidation() {
     var Pass = document.getElementById("pass").value;
     var User = document.getElementById("user").value;
 
-    if ((User !== 'Marwa') || (Pass !== '123')) {
+    if ((User !== 'Nourhan') || (Pass !== '12345')) {
         alert('Invalid Password or Invalid Username!!');
         return false;
     }
